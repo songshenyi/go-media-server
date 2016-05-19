794f58a12385f1997cf9153cbf6ea07771b74b00
Shenyi Song <quickbasic411@gmail.com>
2016-05-19 23:55:30 +0800

    add http server

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	log "github.com/cihub/seelog"
+)
 
 func LiveHandler(w http.ResponseWriter, r *http.Request){
-
+	log.Debug(r.Method)
 }
 
 func main(){
